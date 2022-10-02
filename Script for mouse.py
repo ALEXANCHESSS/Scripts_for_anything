c692dd085166204321bc8f59bfab2fe6a24ca155
Alexander <kuznetsov_alex95@mail.ru>
2022-10-02 15:23:28 +0400

    [script for mouse]

---
+++ Script for mouse.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+
+while True:
+    pyautogui.moveTo(100, 100, duration=0.25)
+    pyautogui.moveTo(200, 100, duration=0.25)
+    pyautogui.moveTo(200, 200, duration=0.25)
+    pyautogui.moveTo(100, 200, duration=0.25)
+    time.sleep(5*60)
